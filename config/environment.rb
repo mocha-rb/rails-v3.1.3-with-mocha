767e6e76b7a026565c4e0ae84f53eabc6f564012
James Mead <james@floehopper.org>
2016-06-03 15:14:41 +0100

    Vanilla Rails v3.1.3

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+RailsV313WithMocha::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsV313WithMocha::Application.config.secret_token = '2be838b93bf9eb2fb6d2c34d52a633412a1063e30dc45992b4cd695c8c2d94c7abc0a644e054a770e4aefccf73644c6f9a4a25acdd76052266ad857e03a7f35b'
